538d14eb41508f6a8617c18a9be28f7914d5990f
luu1003 <juanjot06@gmail.com>
2025-03-24 16:27:52 -0500

    Cambio color fondo y agregar imagenes

--- app/(views)/_layout.tsx
+++ app/(views)/_layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Tabs } from "expo-router";
-import { Platform } from "react-native";
+import { Platform, Image } from "react-native";
 
 export default function ViewsLayout() {
   return (
@@ -10,6 +10,12 @@ export default function ViewsLayout() {
         tabBarPosition: Platform.OS === "web" ? "top" : "bottom",
         tabBarStyle: {
           height: 50,
+          backgroundColor: "#333",
+        },
+        tabBarLabelStyle: {
+          fontSize: 23,
+          fontWeight: "bold",
+          color: "#fff",
         },
       }}
     >
@@ -20,6 +26,8 @@ export default function ViewsLayout() {
           tabBarIcon: () => <></>,
           tabBarItemStyle: {
             flex: 1,
+            alignItems: "flex-start",
+            paddingLeft: 2, // Pegado a la izquierda
           },
         }}
       />
@@ -30,6 +38,8 @@ export default function ViewsLayout() {
           tabBarIcon: () => <></>,
           tabBarItemStyle: {
             flex: 1,
+            alignItems: "flex-start",
+            paddingLeft: 2 // Menor separación respecto a "Inicio"
           },
         }}
       />
@@ -37,11 +47,17 @@ export default function ViewsLayout() {
         name={"shopping-cart/index"}
         options={{
           title: "",
-          //   tabBarIcon: () => (<></>),
+          tabBarIcon: () => (
+            <Image 
+              source={require("../../assets/images/carrito.png")} 
+              style={{ width: 30, height: 30, marginRight: 20 }} 
+            />
+          ),
           tabBarItemStyle: {
-            flex: 6,
+            flex: 0.5,
             justifyContent: "flex-end",
             alignItems: "flex-end",
+            paddingLeft: 2,
           },
         }}
       />
